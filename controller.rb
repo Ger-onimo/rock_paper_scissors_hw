fac2efab3fc2b723d70bdabc340721e0939e5529
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-12-11 21:14:01 +0000

    MVP complete and playing about with html

---
+++ controller.rb
@@ -0,0 +1,19 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry')
+require_relative('models/game')
+also_reload('./models/*')
+
+
+get '/home' do
+  erb(:home)
+end
+
+get '/welcome' do
+  erb(:welcome)
+end
+
+get '/check_win/:hand1/:hand2' do
+  @winner = Game.check_win( params[:hand1], params[:hand2])
+  erb(:result)
+end
